16717166fdcf20b9db7f84b78df7aca51f1a7a7e
dgorjup <domen_gorjup@hotmail.com>
2015-08-03 22:59:07 +0200

    Dome basic HTML templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'Domen'}
+	posts = [{
+				'author':{'nickname':'Ana'},
+				'body':'Domen, ki si ti!'
+				},
+				{
+				'author':{'nickname':'Domen'},
+				'body':'tule, ki si TI?!'
+				},
+				{
+				'author':{'nickname':'David'},
+				'body':'pa ti je ke?'
+				}]
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts=posts)
